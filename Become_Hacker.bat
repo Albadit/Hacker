4510c8e2d22939b348f99c4614bffc8d40f3301c
Albadit <ardit-nl@outlook.com>
2020-02-02 15:33:53 +0100

    new shit

---
+++ Become_Hacker.bat
@@ -0,0 +1,15 @@
+@echo off
+color 2
+
+echo  _                _             
+echo ^| ^|              ^| ^|            
+echo ^| ^|__   __ _  ___^| ^| _____ _ __ _ __ ___   __ _ _ __  
+echo ^| '_ \ / _` ^|/ __^| ^|/ / _ \ '__^| '_ ` _ \ / _` ^| '_ \ 
+echo ^| ^| ^| ^| (_^| ^| (__^|   ^<  __/ ^|  ^| ^| ^| ^| ^| ^| (_^| ^| ^| ^| ^|
+echo ^|_^| ^|_^|\__,_^|\___^|_^|\_\___^|_^|  ^|_^| ^|_^| ^|_^|\__,_^|_^| ^|_^|
+
+timeout /t 3
+
+start "chrome.exe" "https://emeraldworkers.itch.io/hacker-screen/devlog/83427/update-v122"
+
+pause
